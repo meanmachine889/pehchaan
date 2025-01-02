94af6687f8251cebb4675566f2ff65657a6d8108
meanmachine889 <ybharadwaj131@gmail.com>
2025-01-03 02:01:02 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Youtube from "./components/youtube";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-[15vw] md:gap-[3vw] w-screen min-h-screen items-center justify-center bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div className="flex flex-col gap-[15vw] md:gap-[3vw] max-w-screen min-h-screen items-center justify-center bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
       <Hero />
       <Shop/>
       <Youtube />
